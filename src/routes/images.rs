1dbf8e09a48cdd4b5a4279ed85a774ace152aa26
ben <benwillis900@gmail.com>
2025-07-14 09:08:14 +0100

    updates to the static files and added a delete

--- src/routes/images.rs
+++ src/routes/images.rs
@@ -1,15 +1,23 @@
 use axum::{
-    body::Body, extract::{Multipart, Path, State}, http::{header, StatusCode}, response::{Html, IntoResponse, Response}, routing::{get, post}, Router
+    body::Body,
+    extract::{Multipart, Path, State},
+    http::{header, StatusCode}, 
+    response::{Html, IntoResponse, Response},
+    routing::{delete, get, post},
+    Router
 };
-use tokio::fs::File;
-use std::{ fs, io::Write, sync::Arc};
+use mozjpeg::{ColorSpace, Compress};
+use std::{fs, io::Write, sync::Arc};
 use tera::{Context, Tera};
+use tokio::fs::File;
 use tokio_util::io::ReaderStream;
 use uuid::Uuid;
-use mozjpeg::{ ColorSpace, Compress};
+
+const IMAGE_SIZE: usize = 40;
 
 pub fn routes() -> Router<Arc<Tera>> {
     Router::new()
+        .route("/image/{filename}", delete(delete_image))
         .route("/upload", post(upload))
         .route("/images", get(images_home))
         .route("/gallery", get(serve_image_gallery))
@@ -36,7 +44,7 @@ async fn upload(mut multipart: Multipart) -> impl IntoResponse {
         let unique_name = format!(
             "images/{}_{}",
             Uuid::new_v4(),
-            sanitize_filename::sanitize(&file_name) 
+            sanitize_filename::sanitize(&file_name)
         );
 
         if let Err(e) = std::fs::create_dir_all("images") {
@@ -47,9 +55,9 @@ async fn upload(mut multipart: Multipart) -> impl IntoResponse {
         }
         let compressed_image = std::panic::catch_unwind(|| -> std::io::Result<Vec<u8>> {
             let mut comp = Compress::new(ColorSpace::JCS_RGB);
-            comp.set_size(50, 50);
+            comp.set_size(IMAGE_SIZE, IMAGE_SIZE);
             let mut comp = comp.start_compress(data.to_vec())?;
-            let pixels = vec![0u8; 50 * 50 * 3];
+            let pixels = vec![0u8; IMAGE_SIZE * IMAGE_SIZE * 3];
             comp.write_scanlines(&pixels[..])?;
             let writer = comp.finish()?;
             Ok(writer)
@@ -59,22 +67,37 @@ async fn upload(mut multipart: Multipart) -> impl IntoResponse {
             Ok(Ok(compressed_data)) => {
                 let output_path = std::path::Path::new(&unique_name);
                 match std::fs::File::create(output_path)
-                    .and_then(|mut file|file.write_all(&compressed_data))
-                    {
-                        Ok(_) => return (StatusCode::OK, format!("file saved as {}", unique_name)),
-                        Err(e) => return (StatusCode::INTERNAL_SERVER_ERROR, format!("failed to save image {}", e)),
+                    .and_then(|mut file| file.write_all(&compressed_data))
+                {
+                    Ok(_) => return (StatusCode::OK, format!("file saved as {}", unique_name)),
+                    Err(e) => {
+                        return (
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            format!("failed to save image {}", e),
+                        );
                     }
+                }
             }
-            Ok(Err(e)) => return(StatusCode::INTERNAL_SERVER_ERROR, format!("failed compressing image {}", e)),
-            Err(_) => return(StatusCode::INTERNAL_SERVER_ERROR, format!("Panic!! image compression")),
+            Ok(Err(e)) => {
+                return (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("failed compressing image {}", e),
+                );
+            }
+            Err(_) => {
+                return (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Panic!! image compression"),
+                );
+            }
         }
     }
-            (
+    (
         StatusCode::BAD_REQUEST,
         "No file field found in multipart body".to_string(),
     )
 }
-    
+
 async fn images_home(State(tera): State<Arc<Tera>>) -> impl IntoResponse {
     let ctx = Context::new();
     match tera.render("home.html", &ctx) {
@@ -86,21 +109,20 @@ async fn images_home(State(tera): State<Arc<Tera>>) -> impl IntoResponse {
     }
 }
 
-async fn serve_all_images() -> Result<Vec<String>, std::io::Error>
-{
+async fn serve_all_images() -> Result<Vec<String>, std::io::Error> {
     let mut images = Vec::new();
-    let entries =   fs::read_dir("images")?;
+    let entries = fs::read_dir("images")?;
 
-    for entry in entries{
-       let entry = entry?;
-       let path = entry.path();
+    for entry in entries {
+        let entry = entry?;
+        let path = entry.path();
 
-       if path.is_file() {
-           if let Some(file_name) = path.file_name().and_then(|s| s.to_str()){
-               images.push(file_name.to_string());
-           }
-       }
-    }   
+        if path.is_file() {
+            if let Some(file_name) = path.file_name().and_then(|s| s.to_str()) {
+                images.push(file_name.to_string());
+            }
+        }
+    }
     Ok(images)
 }
 
@@ -131,12 +153,15 @@ async fn serve_image(Path(filename): Path<String>) -> Result<Response, StatusCod
 async fn serve_image_gallery() -> Result<Html<String>, StatusCode> {
     match serve_all_images().await {
         Ok(images) => {
-            let mut html = String::from(r#"
+            let mut html = String::from(
+                r#"
                 <div id="image-gallery" class="row">
-            "#);
-            
+            "#,
+            );
+
             for image in images {
-                html.push_str(&format!(r#"
+                html.push_str(&format!(
+                    r#"
                     <div class="col-md-6 col-sm-12 col-xl-4">
                         <img src="/images/{}" alt="{}" class="img-thumbnail" 
                              onclick="openModal(this.src, this.alt)">
@@ -144,12 +169,22 @@ async fn serve_image_gallery() -> Result<Html<String>, StatusCode> {
                             <p class="text-sm text-gray-600 truncate">{}</p>
                         </div>
                     </div>
-                "#, image, image, image));
+                "#,
+                    image, image, image
+                ));
             }
-            
+
             html.push_str("</div>");
             Ok(Html(html))
         }
         Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
+    }
+}
+
+async fn delete_image(Path(filename): Path<String>) -> Result<Html<String>, StatusCode> {
+    let file_path = format!("./images/{}", filename);
+    match std::fs::remove_file(file_path) {
+        Ok(_) => todo!(),
+        Err(_) => todo!(),
     }
 }
